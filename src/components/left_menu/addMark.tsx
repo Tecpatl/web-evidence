24cca66a4cbc874d0d7b2a52c3d77ae7e43cafff
tecpatl <zldarkist@126.com>
2023-11-09 14:29:28 +0800

    bugfix addMarkId end of line

--- src/components/left_menu/addMark.tsx
+++ src/components/left_menu/addMark.tsx
@@ -83,7 +83,7 @@ export default memo(function AddMarkView(props: AddMarkProps) {
                 let res = ""
                 const len = arr.length
                 for (let i = 0; i < len; i++) {
-                  res += "[" + i.toString() + "] " + arr[i]
+                  res += arr[i] + " [" + i.toString() + "] \n"
                 }
                 res = res.replace(/\\n/g, "\n");
                 props.update_format_content_foo(res)
