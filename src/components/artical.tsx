1f7db8d8c65b587f05698aff2fb3b80476ba6876
tecpatl <zldarkist@126.com>
2023-11-09 14:18:26 +0800

    bugfix fontSize

--- src/components/artical.tsx
+++ src/components/artical.tsx
@@ -1,24 +1,22 @@
 import { useState, memo, useEffect, useRef, ReactDOM, ReactNode } from "react";
-import {
-  Button, Divider,
-} from "antd";
+import { Button, Divider } from "antd";
 import MarkdownView from "./markdown";
 
 interface ArticalProps {
   content: string;
-  font_size: number
-  force_flush_idx: number
-  mark_id?: number
+  font_size: number;
+  force_flush_idx: number;
+  mark_id?: number;
 }
 
 export default memo(function ArticalView(props: ArticalProps) {
   const markIdRef = useRef(null);
-  const [markId, setMarkId] = useState<number>()
-  const [contentArrayView, setContentArrayView] = useState<ReactNode[]>()
+  const [markId, setMarkId] = useState<number>();
+  const [contentArrayView, setContentArrayView] = useState<ReactNode[]>();
 
   const divider = /======{\[(\d+)\]}======/g;
 
-  const tmp_str = "{[[<&&&>]]}"
+  const tmp_str = "{[[<&&&>]]}";
 
   const tmp_divider = tmp_str + "======{[$1]}======" + tmp_str;
 
@@ -27,50 +25,52 @@ export default memo(function ArticalView(props: ArticalProps) {
       markIdRef.current.scrollIntoView({
         behavior: "smooth",
       });
-      setMarkId(props.mark_id)
+      setMarkId(props.mark_id);
     } else {
-      window.scrollTo(0, 0)
+      window.scrollTo(0, 0);
     }
-  }
+  };
 
   useEffect(() => {
-    jumpToMarkId()
-  }, [markId?.toString() + props.mark_id?.toString() + props.force_flush_idx?.toString()])
+    jumpToMarkId();
+  }, [
+    markId?.toString() +
+    props.mark_id?.toString() +
+    props.force_flush_idx?.toString(),
+  ]);
 
   useEffect(() => {
     if (!props.content) {
-      window.scrollTo(0, 0)
-      return
+      window.scrollTo(0, 0);
+      return;
     }
-    const formatContent = props.content.replace(divider, tmp_divider)
-    const splittedContent = formatContent.split(tmp_str)
-    const res: ReactNode[] = []
-    const len = splittedContent.length
-    let mark_str = ""
+    const formatContent = props.content.replace(divider, tmp_divider);
+    const splittedContent = formatContent.split(tmp_str);
+    const res: ReactNode[] = [];
+    const len = splittedContent.length;
+    let mark_str = "";
     if (props.mark_id != undefined) {
       mark_str = "======{[" + props.mark_id?.toString() + "]}======";
     }
     for (let i = 0; i < len; i++) {
-      const str = splittedContent[i]
+      const str = splittedContent[i];
       if (mark_str == str) {
         res.push(
-          <div ref={markIdRef} style={{ color: 'red' }} key={i}>{str}</div>
-        )
+          <div ref={markIdRef} style={{ color: "red" }} key={i}>
+            {str}
+          </div>,
+        );
       } else {
         res.push(
-          <MarkdownView
-            key={i}
-            content={str}
-            font_size={props.font_size}
-          />
-        )
+          <MarkdownView key={i} content={str} font_size={props.font_size} />,
+        );
       }
       res.push(
-        <Divider key={"divider" + i} style={{ border: '3px solid red' }} />
-      )
+        <Divider key={"divider" + i} style={{ border: "3px solid red" }} />,
+      );
     }
-    setContentArrayView(res)
-  }, [props.content])
+    setContentArrayView(res);
+  }, [props.content + props.font_size]);
 
   return (
     <div
@@ -79,7 +79,7 @@ export default memo(function ArticalView(props: ArticalProps) {
         overflowY: "auto",
         padding: "10px",
         wordWrap: "break-word",
-        fontSize: props.font_size
+        fontSize: props.font_size,
       }}
     >
       {contentArrayView}
